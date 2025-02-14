4edf7d205fb60ac1a0274cdf0597f428f1edee5e
timfemey <isholaobafemi@gmail.com>
2025-02-14 14:13:18 +0100

    internal validator package

---
+++ helper/validator/validator.go
@@ -0,0 +1,64 @@
+package validator
+
+import "net/url"
+
+//Returns the value as type string and true if its a string
+func IsString(value any, expectedStringsInValue []string) (string, bool) {
+	if value == nil {
+		return "", false
+	}
+	str, isString := value.(string)
+	if !isString {
+		return "", false
+	}
+
+	if len(expectedStringsInValue) > 0 {
+		for _, allowed := range expectedStringsInValue {
+			if str == allowed {
+				return allowed, true
+			}
+		}
+		return "", false
+	}
+
+	return str, isString
+}
+
+func IsURL(value any) (*url.URL, bool) {
+	if value == nil {
+		return nil, false
+	}
+
+	str, ok := value.(string)
+	if !ok {
+		return nil, false
+	}
+
+	parsedURL, err := url.Parse(str)
+	if err != nil {
+		return nil, false
+	}
+
+	if parsedURL.Scheme == "" || parsedURL.Host == "" {
+		return nil, false
+	}
+
+	return parsedURL, true
+}
+
+func IsArray(value any) ([]any, bool) {
+	if value == nil {
+		return nil, false
+	}
+
+	arr, ok := value.([]any)
+	if !ok {
+		return nil, false
+	}
+
+	if len(arr) == 0 {
+		return nil, false
+	}
+
+	return arr, true
+}
